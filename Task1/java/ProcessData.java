66edddc8258ebe4cd79df9d653be4a527b5dbbd1
dakong <dakong94@gmail.com>
2016-04-14 23:31:59 -0700

    test

--- Task1/java/ProcessData.java
+++ Task1/java/ProcessData.java
@@ -1,29 +1,55 @@
-import java.text.SimpleDateFormat;
 import java.util.*;
+import java.io.*;
 
-class ProcessData {
-  public static void main(String[] args) {
+class ProcessData{
 
-    //Initialize Date and colors
-    String timeStamp = (new java.util.Date()).toLocaleString();
-    String[] colors  = {"aqua", "black", "blue", "fuchsia", "gray", "green", "lime",
-        "maroon", "navy", "olive","purple", "red", "silver", "teal", "white", "yellow"};
-    String textColor = "black";
-
-    //Grab random number from 0 -15
-    Random rand = new Random();
-    int randomNum = rand.nextInt(16);
-
-    String backgroundColor  = colors[randomNum];
-
-    if(backgroundColor == "black" || backgroundColor == "navy") textColor = "white";
-    String document = String.format("<!doctype html> \n"
-      + "<body style = 'background-color: %s'>"
-      + "<h1 style = 'color:%s'> Hello World from Java @ %s </h1>"
-      + "</body> "
-      +"</html>", backgroundColor , textColor ,  timeStamp);
+  static void toHTML(String content){
+    String html = String.format("<!doctype html><html><body><head><title>Process Data Java</title></head>%s</body></html>",content);
 
     System.out.print("Content-Type: text/html\n\n");
-    System.out.print(document);
+    System.out.println(html);
+  }
+  public static void main(String[] args){
+
+    Map<String, String> formData = new HashMap <String, String>();
+    Map<String, String> env = System.getenv();
+    String data = "username=danielkong&password=helloworld&magicnumber=5";
+    //String data = "";
+    String htmlContent = "";
+    if(env.get("REQUEST_METHOD") == "GET"){
+       data = env.get("QUERY_STRING");
+    }
+    else if(env.get("REQUEST_METHOD") == "POST") {
+      Console console = System.console();
+      String line;
+      while((line = console.readLine()) != null){
+        data = data + line;
+      }
+    }
+    else{
+      toHTML("<h1>There was no GET or POST request submitted through a form!</h1>");
+      return;
+    }
+
+    String[] formField = data.split("&");
+
+    for(int i = 0; i < formField.length; i++){
+      String[] fields = formField[i].split("=");
+      formData.put(fields[0],fields[1]);
+    }
+    int magicNum;
+    try{
+      magicNum = Integer.parseInt(formData.get("magicnumber"));
+    }catch(NumberFormatException e){
+      toHTML("<h1> Error parsing magic number </h1>");
+      return;
+    }
+
+    for(int i = 0; i < magicNum; i++){
+      htmlContent = htmlContent + ("<h1> Hello " + formData.get("username") + " with a password of " + formData.get("password") + " </h1>");
+    }
+
+    toHTML("Hello world");
+
   }
 }
