78e446aec5aea2f9e8d7661f9b04049f75f8c85a
dakong <dakong94@gmail.com>
2016-04-14 22:27:14 -0700

    CGI form

--- Task1/java/GetPost.java
+++ Task1/java/GetPost.java
@@ -3,51 +3,27 @@ import java.io.*;
 
 class GetPost{
 
-  static void toHTML(String content){
-    String html = String.format("<Content-Type: text/html>\n\n <!doctype html><html><body><head><title>Process Data Java</title></head>%s</body></html>",content);
-    System.out.println(html);
-  }
+
   public static void main(String[] args){
-
-    Map<String, String> formData = new HashMap <String, String>();
-    Map<String, String> env = System.getenv();
-    //String data = "username=danielkong&password=helloworld&magicnumber=5";
-    String data = "";
-    String htmlContent = "";
-    if(env.get("REQUEST_METHOD") == "GET"){
-       data = env.get("QUERY_STRING");
-    }
-    else if(env.get("REQUEST_METHOD") == "POST") {
-      Console console = System.console();
-      String line;
-      while((line = console.readLine()) != null){
-        data = data + line;
-      }
-    }
-    else{
-      toHTML("<h1>There was no GET or POST request submitted through a form!</h1>");
-      return;
-    }
-
-    String[] formField = data.split("&");
-
-    for(int i = 0; i < formField.length; i++){
-      String[] fields = formField[i].split("=");
-      formData.put(fields[0],fields[1]);
-    }
-    int magicNum;
-    try{
-      magicNum = Integer.parseInt(formData.get("magicnumber"));
-    }catch(NumberFormatException e){
-      toHTML("<h1> Error parsing magic number </h1>");
-      return;
-    }
-
-    for(int i = 0; i < magicNum; i++){
-      htmlContent = htmlContent + ("<h1> Hello " + formData.get("username") + " with a password of " + formData.get("password") + " </h1>");
-    }
-
-    toHTML(htmlContent);
+    System.out.println( "Content-Type: text/html\n");
+    System.out.println( "<html>\n<head>\n<title>Get And Post Forms</title>");
+    System.out.println( "<style>" );
+    System.out.println( "</style>" );
+    System.out.println( "<body>");
+    System.out.println( "<h1>GET Form </h1>" );
+    System.out.println( "<form action='processdata.cgi' method='get'>");
+    System.out.println( "<input type='text' name='username' placeholder='Username'>" );
+    System.out.println( "<input type='password' name='password' placeholder='Password'>" );
+    System.out.println( "<input type='number' name='magicnumber' placeholder='Magic Number'>" );
+    System.out.println( "<input type='submit' value='Submit'>" );
+    System.out.println( "</form>" );
+    System.out.println( "<h1>Post Form </h1>" );
+    System.out.println( "<form action='processdata.cgi' method='post'>");
+    System.out.println( "<input type='text' name='username' placeholder='Username'>" );
+    System.out.println( "<input type='password' name='password' placeholder='Password'>" );
+    System.out.println( "<input type='number' name='magicnumber' placeholder='Magic Number'>" );
+    System.out.println( "<input type='submit' value='Submit'>" );
+    System.out.println( "</body>\n</html>" );
 
   }
 }
---
+++ Task1/java/ProcessData.java
@@ -0,0 +1,53 @@
+import java.util.*;
+import java.io.*;
+
+class ProcessData{
+
+  static void toHTML(String content){
+    String html = String.format("<Content-Type: text/html>\n\n <!doctype html><html><body><head><title>Process Data Java</title></head>%s</body></html>",content);
+    System.out.println(html);
+  }
+  public static void main(String[] args){
+
+    Map<String, String> formData = new HashMap <String, String>();
+    Map<String, String> env = System.getenv();
+    //String data = "username=danielkong&password=helloworld&magicnumber=5";
+    String data = "";
+    String htmlContent = "";
+    if(env.get("REQUEST_METHOD") == "GET"){
+       data = env.get("QUERY_STRING");
+    }
+    else if(env.get("REQUEST_METHOD") == "POST") {
+      Console console = System.console();
+      String line;
+      while((line = console.readLine()) != null){
+        data = data + line;
+      }
+    }
+    else{
+      toHTML("<h1>There was no GET or POST request submitted through a form!</h1>");
+      return;
+    }
+
+    String[] formField = data.split("&");
+
+    for(int i = 0; i < formField.length; i++){
+      String[] fields = formField[i].split("=");
+      formData.put(fields[0],fields[1]);
+    }
+    int magicNum;
+    try{
+      magicNum = Integer.parseInt(formData.get("magicnumber"));
+    }catch(NumberFormatException e){
+      toHTML("<h1> Error parsing magic number </h1>");
+      return;
+    }
+
+    for(int i = 0; i < magicNum; i++){
+      htmlContent = htmlContent + ("<h1> Hello " + formData.get("username") + " with a password of " + formData.get("password") + " </h1>");
+    }
+
+    toHTML(htmlContent);
+
+  }
+}
