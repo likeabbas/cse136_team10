992481c537fb4701405092ec4b07bbe318f9ff38
GuillaumeHauss <ghauss@hotmail.fr>
2016-04-16 16:59:34 -0700

    Create delete.php

---
+++ Task2/delete.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="style.css">
+	<title>Delete a Bookmark</title>
+</head>
+<body>
+  <div class="container">
+    <h2>Bookmarx</h2>
+    <br/>
+    <p>Are you sure you want to delete this Bookmark?
+    <br/>Title : 
+    <br/>URL : 
+    <br/>Keywords : 
+    </p>
+  </div>
+</body>
+</html>
