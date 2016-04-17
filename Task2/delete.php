69a67c7e18ff51dbb6b7037b0a4a11537ab10711
GuillaumeHauss <ghauss@hotmail.fr>
2016-04-17 14:47:39 -0700

    Update delete.php

--- Task2/delete.php
+++ Task2/delete.php
@@ -10,9 +10,9 @@
   <div class="container">
 	    <h2>Bookmarx</h2>
 	    <p><b>Are you sure you want to delete this Bookmark?</b></p>
-	    <p>Title :</p>
-	    <p>URL :</p>
-	    <p>Keywords :</p>
+	    <p>Title :<i>Title of the selected bookmark in the previous page</i></p>
+	    <p>URL :<i>URL of the selected bookmark</i></p>
+	    <p>Keywords :<i>Keywords of the selected bookmark</i></p>
     <form id="delete-button-wrapper" style ="overflow:auto;"action="perform_delete.php" method="post">
     	<button type="submit" id="confirm_delete">Yes</button>
 			<button type="button" onClick = "location.href='assignment2.html'"id="cancel_delete">No</button>
