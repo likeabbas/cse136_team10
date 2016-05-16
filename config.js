e54e6343dd7fb4f969e90b2b34cd2100f5aee28e
Abbas Dharamsey <adharams@ucsd.edu>
2016-05-16 15:17:53 -0700

    adding config

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  DATABASE_HOST: 'localhost',
+  DATABASE_USER: 'dev',
+  DATABASE_PASSWORD: '2Eo240J3FTuDs80UBJPX',
+  DATABASE_NAME: 'dev',
+  PORT: 8080,
+  USERNAME: 'root',
+  PASSWORD: 'password'
+};
