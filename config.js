a14cb1041469e6bc657c9e26e20dbcc449376c7f
AndersTrier <anders.trier.olesen@gmail.com>
2016-05-16 19:51:51 -0700

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    DATABASE_HOST: 'localhost',
-    DATABASE_USER: 'root',
-    DATABASE_PASSWORD: 'password',
-    DATABASE_NAME: 'dev',
-    PORT: 8080,
-    USERNAME: 'root',
-    PASSWORD: 'password'
-};
